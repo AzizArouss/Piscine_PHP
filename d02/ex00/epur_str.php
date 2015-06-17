b8a77277c2d3ff95f8d4aa85178874a0e3aecca5
Aziz AROUSS <aarouss@e1r6p8.42.fr>
2015-06-17 14:32:00 +0200

    test

--- d02/ex00/epur_str.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-$str = trim($argv[1]);									//Supprime les espaces en debut et fin de chaine
-if ($str==NULL)											//Si on ne rentre aucune valeur
-	return;												//Alors on retourne rien du tout o/
-$str = preg_replace('/\s\s+/', ' ', $str);				//Supprime les espaces en trop dans une chaine
-echo("$str\n");											//Retour a la ligne
-?>
